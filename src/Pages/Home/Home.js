3cf91145f0f1e53ace01e2fc7ebaf2feb4513e45
gprva <148754074+gprva@users.noreply.github.com>
2024-06-01 19:10:07 +0600

    delete img

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,16 +1,7 @@
 import "./Home.css";
-import hoverImg1 from "../../img/hov-card1.jpg"
 import golfImg from "../../img/Golf.jpg"
 import soccerImg from "../../img/soccer.jpg"
-import laptop from "../../img/laptop-fr.jpeg"
-import smartwatches from "../../img/smartwatched.jpg"
-import headphones from "../../img/zakrytye.jpg"
-import phone from "../../img/phone.jpg"
-import slideImg from "../../img/phone-for-slid.avif"
-import slideImg2 from "../../img/phone2.jpg"
-import slideImg3 from "../../img/phone3.webp"
-import slideImg4 from "../../img/phone4.png"
-import slideImg1 from "../../img/phone1.png"
+import hoverImg1 from "../../img/voll.png"
 
 function Home() {
   
@@ -32,7 +23,7 @@ function Home() {
         <div className="hover_cards flex">
 
         <div class="hover-effect-btn">
-          <img src={hoverImg1} alt="" />
+          <img className="vol_img" src={hoverImg1} alt="" />
           <h3 class="title">Volleyball</h3>
           <div class="overlay"></div>
         <div class="button"><a href="#">shop</a></div>
